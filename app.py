38ab780bf6ac546c0fa7d39e669725f23801a2cd
sawloy <44690795+sawloy@users.noreply.github.com>
2025-07-02 00:25:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,7 @@
-from flask import Flask, jsonify
-import pandas as pd
-import requests
-from datetime import datetime
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/get_wbgt")
 def get_wbgt():
-    url = "https://www.wbgt.env.go.jp/graph_data/td_data_82056.csv"
-    df = pd.read_csv(url, encoding='shift_jis')
-
-    now_hour = datetime.now().strftime("%H:00")
-    current_row = df[df["時刻"] == now_hour]
-    if not current_row.empty:
-        wbgt = current_row.iloc[0]["暑さ指数(WBGT)"]
-        return jsonify({"wbgt": str(wbgt)})
-    else:
-        return jsonify({"wbgt": "N/A"})
+    return {"wbgt": "27"}
